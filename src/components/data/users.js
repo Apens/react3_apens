33d156725e412896f30fcb20bfc38775dafc1f0d
laurent <l.neveux@icloud.com>
2021-05-25 15:05:12 +0200

    Add Users Data

---
+++ src/components/data/users.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    id: 1,
+    name: 'Laurent',
+    job: 'Dev',
+    address: {
+      line1: 'Boulevard adresse',
+      line2: 'adresse 2',
+    },
+    phones: ['07-07-070-7', '064512178'],
+  },
+  {
+    id: 2,
+    name: 'Patrice',
+    job: 'Dev',
+    address: {
+      line1: 'Boulevard adresse',
+      line2: 'adresse 2',
+    },
+    phones: ['07-07-070-7', '064512178'],
+  },
+  {
+    id: 3,
+    name: 'Evens',
+    job: 'Dev',
+    address: {
+      line1: 'Boulevard adresse',
+      line2: 'adresse 2',
+    },
+    phones: ['07-07-070-7', '064512178'],
+  },
+];
