3d56b4c441ed54e1e8979b34cd26057c2fc06ced
laurent <l.neveux@icloud.com>
2021-05-25 11:54:02 +0200

    Add data

---
+++ src/components/data/index.js
@@ -0,0 +1,22 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Titre article 1',
+    body: 'Contenu Article 1',
+  },
+  {
+    id: 2,
+    title: 'Titre article 2',
+    body: 'Contenu Article 2',
+  },
+  {
+    id: 3,
+    title: 'Titre article 3',
+    body: 'Contenu Article 3',
+  },
+  {
+    id: 4,
+    title: 'Titre article 4',
+    body: 'Contenu Article 4',
+  },
+];
