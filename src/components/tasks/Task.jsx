d4f08f24f9734e41edbe8e4b0b403c69831949a4
laurent <l.neveux@icloud.com>
2021-05-28 11:12:15 +0200

    Steve tente un truc

--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Task = ({ task }) => {
+const Task = ({ task, update }) => {
   return (
     <tr className={'text-primary'}>
       <td>{task.id}</td>
       <td>{task.title}</td>
       <td>{task.done ? 'Bien' : 'Pas bien'}</td>
       <td>
-        <button className={'btn btn-xs btn-warning'}>Done</button>
+        <button
+          className={'btn btn-xs btn-warning'}
+          onClick={() => update(task.id)}
+        >
+          Done ?
+        </button>
       </td>
       <td>
         <Link
--- src/components/tasks/TaskList.jsx
+++ src/components/tasks/TaskList.jsx
@@ -1,27 +1,65 @@
-import React, { useState } from 'react';
-
+import React, { Fragment, useState } from 'react';
+import Text from '../subcomponents/formComponents/Text';
 import tasks from '../data/tasks';
 import Task from './Task';
+
 const TaskList = () => {
   const [tasksList, setTasksList] = useState(tasks);
+  const [taskTitle, setTaskTitle] = useState('');
+
+  //  On doit target, donc on accepte le e
+  const handleChange = e => {
+    setTaskTitle(e.target.value);
+  };
+
+  const addTask = e => {
+    e.preventDefault();
+
+    const task = {
+      id: Math.random().toString(16),
+      title: taskTitle,
+      done: false,
+    };
+
+    setTasksList([task, ...tasksList]);
+  };
+
+  const handleUpdate = id => {
+    // {/*Si on passe un parametre, on a besoin d'un callback (au clique sur le bouton )*/}
+    const task = tasksList.filter(task => task.id === id)[0];
+
+    // setTaskDone(!task.done);
+  };
 
   return (
-    <table className={'table table-striped mb-5'}>
-      <thead>
-        <tr className={'text-info'}>
-          <th>Id</th>
-          <th>Title</th>
-          <th>Done</th>
-          <th>Mark Done ?</th>
-          <th>Voir</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasksList.map(task => {
-          return <Task key={task.id} task={task} />;
-        })}
-      </tbody>
-    </table>
+    <Fragment>
+      <form onSubmit={addTask}>
+        <Text
+          name={'title'}
+          type={'text'}
+          value={taskTitle}
+          onChange={handleChange}
+          placeHolder={'Ajouter une tache'}
+        />
+        <button className={'btn btn-xs btn-success'}>Add Task</button>
+      </form>
+      <table className={'table table-striped mb-5'}>
+        <thead>
+          <tr className={'text-info'}>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Done</th>
+            <th>Mark Done ?</th>
+            <th>Voir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasksList.map(task => {
+            return <Task key={task.id} task={task} update={handleUpdate} />;
+          })}
+        </tbody>
+      </table>
+    </Fragment>
   );
 };
 
